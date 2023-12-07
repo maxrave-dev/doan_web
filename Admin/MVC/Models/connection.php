2df4f01b2f4ea8dda5d8e3e0a5468cebb5fc7b66
Nguyễn Đức Tuấn Minh <minhnguyen.31211027595@st.ueh.edu.vn>
2023-12-07 08:26:19 +0700

    Fix data, Add Leaflet

--- Admin/MVC/Models/connection.php
+++ Admin/MVC/Models/connection.php
@@ -5,13 +5,15 @@
         function __construct()
         {
             //Thong so ket noi CSDL
-            $severname ="localhost"; 
-            $username ="root";
-            $password =""; 
-            $db_name ="shopphone";
+            //Host heroku
+            $severname ="ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+            $username ="vmaa4opycpakkbpn";
+            $password ="rdh3nusdib7eppu9";
+            $db_name ="bgjvxnm3vmpseadd";
+            $port= 3306;
 
             //Tao ket noi CSDL
-            $this->conn = new mysqli($severname,$username,$password,$db_name);
+            $this->conn = new mysqli($severname,$username,$password,$db_name,$port);
             $this->conn->set_charset("utf8");
 
             //check connection
--- Models/connection.php
+++ Models/connection.php
@@ -3,14 +3,14 @@
         var $conn;
         function __construct()
         {
-            //Thong so ket noi CSDL
-            $severname ="localhost"; 
-            $username ="root";
-            $password =""; 
-            $db_name ="shopphone";
- 
+            $severname ="ltnya0pnki2ck9w8.chr7pe7iynqr.eu-west-1.rds.amazonaws.com";
+            $username ="vmaa4opycpakkbpn";
+            $password ="rdh3nusdib7eppu9";
+            $db_name ="bgjvxnm3vmpseadd";
+            $port= 3306;
+
             //Tao ket noi CSDL
-            $this->conn = new mysqli($severname,$username,$password,$db_name);
+            $this->conn = new mysqli($severname,$username,$password,$db_name,$port);
             $this->conn->set_charset("utf8");
 
             //check connection
